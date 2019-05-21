d6a6f5cc8bb0715a2657316ac1c6c308d9260185
alisterg <alister7@gmail.com>
2019-05-21 12:32:07 +1000

    adding visitor pattern

---
+++ DesignPatterns/Behavioural/Visitor.cs
@@ -0,0 +1,10 @@
+namespace DesignPatterns.Behavioural
+{
+    /// <summary>
+    /// TODO make a CakeVisitor that puts icing on the cakes
+    /// </summary>
+    public class Visitor
+    {
+        
+    }
+}
